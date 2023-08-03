75a88920979e81c907b7c53dca69e2e4905db2a0
Oscar Mora <oscgmora@gmail.com>
2023-08-02 22:20:37 -0400

    Complete Display of Weather Information
    This commit completes the display of all weather information needed for the DOM. This also creates a new module to help clear up space in the DOM-Controller module. This new module contains formatting and creating/appending functions for the new elements that are created from the data received by the API.

--- src/modules/dom-controller.js
+++ src/modules/dom-controller.js
@@ -1,96 +1,65 @@
-import { format } from 'date-fns';
+import {
+    formatDate,
+    formatDayOfTheWeek,
+    formatTime,
+    createAndAppendElement
+} from './format-and-create-elements';
 
 const containerOne = document.querySelector('#container-one');
 const containerTwo = document.querySelector('#container-two');
-
-function formatDate(timeData) {
-    const year = timeData.slice(0, 4);
-    const nextMonth = timeData.slice(5, 7);
-    const day = timeData.slice(8, 10);
-
-    // Fix which month displays for Date-Fns
-    const currentMonth = Number(nextMonth) - 1;
-
-    const formattedDate = format(
-        new Date(year, currentMonth, day),
-        'MMMM dd, yyyy'
-    );
-    return formattedDate;
-}
-
-function formatTime(timeData) {
-    const year = timeData.slice(0, 4);
-    const month = timeData.slice(5, 7);
-    const day = timeData.slice(8, 10);
-    let hour = timeData.slice(11, 13);
-    const minutes = timeData.slice(14, 17);
-
-    if (hour.includes(':')) {
-        hour = hour.replace(':', '');
-        hour = `0${hour}`;
-    }
-
-    const formattedTime = format(
-        new Date(year, month, day, hour, minutes),
-        'h:mm aaaa'
-    );
-
-    return formattedTime;
-}
 
 function clearDOM() {
     containerOne.innerHTML = '';
     containerTwo.innerHTML = '';
 }
 
+function loadDailyForecast(forecastDayData) {
+    forecastDayData.forEach((day) => {
+        createAndAppendElement(containerTwo, formatDayOfTheWeek(day.date));
+        createAndAppendElement(containerTwo, `${day.avgtemp_c}°C`);
+        createAndAppendElement(containerTwo, `${day.avgtemp_f}°F`);
+        createAndAppendElement(containerTwo, `${day.rain} %`);
+    });
+}
+
+function loadHourlyForecast(forecastHourlyData) {
+    forecastHourlyData.forEach((day) => {
+        day.forEach((hour) => {
+            createAndAppendElement(containerTwo, formatTime(hour.time));
+            createAndAppendElement(containerTwo, `${hour.temp_c}°C`);
+            createAndAppendElement(containerTwo, `${hour.temp_f}°F`);
+            createAndAppendElement(containerTwo, `${hour.chance_of_rain} %`);
+        });
+    });
+}
+
 function loadContainerOne(reducedWeatherData) {
-    const formattedDate = formatDate(reducedWeatherData.time);
-    const formattedTime = formatTime(reducedWeatherData.time);
-
-    const locationP = document.createElement('h1');
-    const dateP = document.createElement('p');
-    const timeP = document.createElement('p');
-    const condition = document.createElement('p');
-    const tempC = document.createElement('p');
-    const tempF = document.createElement('p');
-    const tempCFeelsLike = document.createElement('p');
-    const tempFFeelsLike = document.createElement('p');
-    const humidity = document.createElement('p');
-    const windKPH = document.createElement('p');
-    const windMPH = document.createElement('p');
-
-    locationP.innerText = reducedWeatherData.name;
-    dateP.innerText = formattedDate;
-    timeP.innerText = formattedTime;
-    condition.innerText = reducedWeatherData.condition;
-    tempC.innerText = `${reducedWeatherData.temp_c}°C`;
-    tempF.innerText = `${reducedWeatherData.temp_f}°F`;
-    tempCFeelsLike.innerText = `${reducedWeatherData.feelslike_c}°C`;
-    tempFFeelsLike.innerText = `${reducedWeatherData.feelslike_f}°F`;
-    humidity.innerText = `${reducedWeatherData.humidity} %`;
-    windKPH.innerText = `${reducedWeatherData.wind_kph} KM/H`;
-    windMPH.innerText = `${reducedWeatherData.wind_mph} MPH`;
-
-    containerOne.append(
-        locationP,
-        dateP,
-        timeP,
-        condition,
-        tempC,
-        tempF,
-        tempCFeelsLike,
-        tempFFeelsLike,
-        humidity,
-        windKPH,
-        windMPH
-    );
+    createAndAppendElement(containerOne, reducedWeatherData.name);
+    createAndAppendElement(containerOne, formatDate(reducedWeatherData.time));
+    createAndAppendElement(containerOne, formatTime(reducedWeatherData.time));
+    createAndAppendElement(containerOne, reducedWeatherData.condition);
+    createAndAppendElement(containerOne, `${reducedWeatherData.temp_c}°C`);
+    createAndAppendElement(containerOne, `${reducedWeatherData.temp_f}°F`);
+    createAndAppendElement(containerOne, `${reducedWeatherData.feelslike_c}°C`);
+    createAndAppendElement(containerOne, `${reducedWeatherData.feelslike_f}°F`);
+    createAndAppendElement(containerOne, `${reducedWeatherData.humidity} %`);
+    createAndAppendElement(containerOne, `${reducedWeatherData.wind_kph} KM/H`);
+    createAndAppendElement(containerOne, `${reducedWeatherData.wind_mph} MPH`);
 }
 
 function loadContainerTwo(reducedWeatherData) {
-    const forecastDayData = reducedWeatherData.forecastDay;
-    forecastDayData.forEach((day) => {
-        console.log(day);
-    });
+    createAndAppendElement(
+        containerTwo,
+        `Sunrise: ${reducedWeatherData.forecastDay[0].sunrise}`
+    );
+    createAndAppendElement(
+        containerTwo,
+        `Sunset: ${reducedWeatherData.forecastDay[0].sunset}`
+    );
+
+    loadDailyForecast(reducedWeatherData.forecastDay);
+
+    loadHourlyForecast(reducedWeatherData.forecastHourly);
 }
 
 function loadDOM(reducedWeatherData) {
---
+++ src/modules/format-and-create-elements.js
@@ -0,0 +1,38 @@
+import { format } from 'date-fns';
+
+function extractDateParts(timeData) {
+    const [year, month, day] = timeData.slice(0, 10).split('-');
+    const currentMonth = Number(month) - 1;
+    return { year, month, day, currentMonth };
+}
+
+function formatDate(timeData) {
+    const { year, currentMonth, day } = extractDateParts(timeData);
+    return format(new Date(year, currentMonth, day), 'EEEE, MMMM d yyyy');
+}
+
+function formatDayOfTheWeek(timeData) {
+    const { year, currentMonth, day } = extractDateParts(timeData);
+    return format(new Date(year, currentMonth, day), 'EEEE');
+}
+
+function formatTime(timeData) {
+    const { year, month, day } = extractDateParts(timeData);
+    let hour = timeData.slice(11, 13);
+    const minutes = timeData.slice(14, 17);
+
+    if (hour.includes(':')) {
+        hour = hour.replace(':', '');
+        hour = `0${hour}`;
+    }
+
+    return format(new Date(year, month - 1, day, hour, minutes), 'h:mm a');
+}
+
+function createAndAppendElement(parent, text) {
+    const element = document.createElement('p');
+    element.innerText = text;
+    parent.appendChild(element);
+}
+
+export { formatDate, formatDayOfTheWeek, formatTime, createAndAppendElement };
