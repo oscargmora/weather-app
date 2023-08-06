c8fba3b2160cb22cf34be1167c4254a7b5c45a4d
Oscar Mora <oscgmora@gmail.com>
2023-08-06 11:31:22 -0400

    Add Background Images

--- src/modules/style-controller.js
+++ src/modules/style-controller.js
@@ -1,36 +1,36 @@
+import dayPartlyCloudy from '../img/backgrounds/day-partly-cloudy.jpg';
+import dayClearSkies from '../img/backgrounds/day-clear-skies.jpg';
+import nightClearSkies from '../img/backgrounds/night-clear-skies.jpg';
+import nightPartlyCloudy from '../img/backgrounds/night-partly-cloudy.jpg';
+import overcast from '../img/backgrounds/overcast.jpg';
+import rain from '../img/backgrounds/rain.jpg';
+import thunderstorm from '../img/backgrounds/thunderstorm.jpg';
+
 function backgroundUpdater(reducedWeatherData) {
     switch (true) {
         case reducedWeatherData.condition === 'Partly cloudy':
-            document.body.style.backgroundImage =
-                "url('../img/backgrounds/day-partly-cloudy.jpg')";
+            document.body.style.backgroundImage = `url('${dayPartlyCloudy}')`;
             break;
         case reducedWeatherData.condition === 'Overcast':
-            document.body.style.backgroundImage =
-                "url('../img/backgrounds/overcast.jpg')";
+            document.body.style.backgroundImage = `url('${overcast}')`;
             break;
         case reducedWeatherData.condition === 'Sunny':
-            document.body.style.backgroundImage =
-                "url('../img/backgrounds/day-clear-skies.jpg')";
+            document.body.style.backgroundImage = `url('${dayClearSkies}')`;
             break;
         case reducedWeatherData.condition === 'Rainy':
-            document.body.style.backgroundImage =
-                "url('../img/backgrounds/rain.jpg')";
+            document.body.style.backgroundImage = `url('${rain.jpg}')`;
             break;
         case reducedWeatherData.condition === 'Thunderstorms':
-            document.body.style.backgroundImage =
-                "url('../img/backgrounds/thunderstorm.jpg')";
+            document.body.style.backgroundImage = `url('${thunderstorm}')`;
             break;
         case reducedWeatherData.condition === 'Partly Cloudy':
-            document.body.style.backgroundImage =
-                "url('../img/backgrounds/night-partly-cloudy.jpg')";
+            document.body.style.backgroundImage = `url('${nightPartlyCloudy}')`;
             break;
         case reducedWeatherData.condition === 'Clear':
-            document.body.style.backgroundImage =
-                "url('../img/backgrounds/night-clear-skies.jpg')";
+            document.body.style.backgroundImage = `url('${nightClearSkies}')`;
             break;
         default:
-            document.body.style.backgroundImage =
-                "url('../img/day-partly-cloudy.jpg')";
+            document.body.style.backgroundImage = `url('${dayClearSkies}')`;
             break;
     }
 }
