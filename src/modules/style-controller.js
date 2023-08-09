7f4346b5cd52a86cc6e0a5d6a59ea817e0b61ed0
Oscar Mora <oscgmora@gmail.com>
2023-08-09 10:14:54 -0400

    Add Form Styling

--- src/modules/style-controller.js
+++ src/modules/style-controller.js
@@ -7,28 +7,50 @@ import rain from '../img/backgrounds/rain.jpg';
 import thunderstorm from '../img/backgrounds/thunderstorm.jpg';
 
 function backgroundUpdater(reducedWeatherData) {
+    const input = document.querySelector('input');
+    const buttons = document.querySelectorAll('button');
     switch (true) {
         case reducedWeatherData.condition === 'Partly cloudy' &&
             reducedWeatherData.is_day === 0:
             document.body.style.backgroundImage = `url('${nightPartlyCloudy}')`;
             document.body.classList.add('dark-mode');
+            input.classList.add('dark-mode');
+            buttons.forEach((button) => {
+                button.classList.add('dark-mode');
+            });
             break;
         case reducedWeatherData.condition === 'Clear' &&
             reducedWeatherData.is_day === 0:
             document.body.style.backgroundImage = `url('${nightClearSkies}')`;
             document.body.classList.add('dark-mode');
+            input.classList.add('dark-mode');
+            buttons.forEach((button) => {
+                button.classList.add('dark-mode');
+            });
             break;
         case reducedWeatherData.condition === 'Partly cloudy':
             document.body.style.backgroundImage = `url('${dayPartlyCloudy}')`;
             document.body.classList.remove('dark-mode');
+            input.classList.remove('dark-mode');
+            buttons.forEach((button) => {
+                button.classList.remove('dark-mode');
+            });
             break;
         case reducedWeatherData.condition === 'Overcast':
             document.body.style.backgroundImage = `url('${overcast}')`;
             document.body.classList.add('dark-mode');
+            input.classList.add('dark-mode');
+            buttons.forEach((button) => {
+                button.classList.add('dark-mode');
+            });
             break;
         case reducedWeatherData.condition === 'Sunny':
             document.body.style.backgroundImage = `url('${dayClearSkies}')`;
             document.body.classList.remove('dark-mode');
+            input.classList.remove('dark-mode');
+            buttons.forEach((button) => {
+                button.classList.remove('dark-mode');
+            });
             break;
         case reducedWeatherData.condition === 'Rainy' ||
             reducedWeatherData.condition === 'Patchy rain possible' ||
@@ -36,15 +58,27 @@ function backgroundUpdater(reducedWeatherData) {
             reducedWeatherData.condition === 'Moderate rain':
             document.body.style.backgroundImage = `url('${rain}')`;
             document.body.classList.add('dark-mode');
+            input.classList.add('dark-mode');
+            buttons.forEach((button) => {
+                button.classList.add('dark-mode');
+            });
             break;
         case reducedWeatherData.condition === 'Thunderstorms' ||
             reducedWeatherData.condition === 'Patchy light rain with thunder':
             document.body.style.backgroundImage = `url('${thunderstorm}')`;
             document.body.classList.add('dark-mode');
+            input.classList.add('dark-mode');
+            buttons.forEach((button) => {
+                button.classList.add('dark-mode');
+            });
             break;
         default:
             document.body.style.backgroundImage = `url('${dayClearSkies}')`;
             document.body.classList.remove('dark-mode');
+            input.classList.remove('dark-mode');
+            buttons.forEach((button) => {
+                button.classList.remove('dark-mode');
+            });
             break;
     }
 }
